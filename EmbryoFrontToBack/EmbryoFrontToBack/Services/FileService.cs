9d23d0120b3758fb72b02f479e403a35a3f72c2d
SadiqNahmetov <sadigfn@code.edu.az>
2022-11-04 02:28:10 +0400

    Fixed: Homework

---
+++ EmbryoFrontToBack/EmbryoFrontToBack/Services/FileService.cs
@@ -0,0 +1,23 @@
+﻿using EmbryoFrontToBack.Services.Interface;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmbryoFrontToBack.Services
+{
+    public class FileService : IFileService
+    {
+        //"wwwroot/assets/templates/verify.html"
+        public string ReadFile(string path, string template)
+        {
+            using (StreamReader reader = new StreamReader(path))
+            {
+                template = reader.ReadToEnd();
+            }
+
+            return template;
+        }
+    }
+}
